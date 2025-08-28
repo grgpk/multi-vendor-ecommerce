cd9e2783755a0da87a3e713ef4a7fbb12517566f
Giorgi <grgpaikidze@gmail.com>
2025-08-28 22:30:54 +0400

    feat: add login and register routes

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
+import Router from "./router/Router";
+
 function App() {
-  return (
-    <h1 className="text-center text-3xl font-bold underline">Hello world!</h1>
-  );
+  const [allRoutes, setAllRoutes] = useState([]);
+
+  return <Router allRoutes={allRoutes} />;
 }
 
 export default App;
